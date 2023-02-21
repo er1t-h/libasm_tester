81fe10fb80e10f21bec1e2fbdf031251b04efb6f
hperrin <hperrin@student.42.fr>
2023-02-21 12:57:58 +0100

    Working on ft_read test

--- src/tests/mandatory.rs
+++ src/tests/mandatory.rs
@@ -22,7 +22,6 @@ mod strlen {
 
 mod strcpy {
     use std::ffi::CString;
-
     use crate::ft_strcpy;
 
 	macro_rules! test {
@@ -47,4 +46,97 @@ mod strcpy {
 	test!(basic, "superTest");
 	test!(longer, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ornare et ipsum et molestie. Sed fermentum metus ut sem imperdiet pretium. Etiam non dolor justo. Nullam dignissim malesuada dui, a malesuada ex facilisis ac. Nullam sit amet odio et neque vestibulum eleifend. Etiam malesuada ultrices orci. Sed quam ligula, pharetra at mattis vitae, mollis et urna. Proin a lobortis elit. Quisque gravida nec lorem ut auctor. In vitae tincidunt arcu. Cras ultricies augue augue, in mattis massa elementum vel.");
 	test!(utf8, "Salut! C'est un test de qualité contenant de supers UTF-8. 🀄麻雀🀄がしたい。このテストは本当に面白いなぁ。");
+}
+
+mod strcmp {
+	use std::ffi::CString;
+    use crate::ft_strcmp;
+
+	macro_rules! test {
+		($name: ident, $to_test: expr) => {
+			#[test]
+			fn $name() {
+				let s1 = CString::new($to_test).expect("Cannot create first string");
+				let s2 = CString::new($to_test).expect("Cannot create second string");
+				let ret_val = unsafe {
+					ft_strcmp(s1.into_raw(), s2.into_raw())
+				};
+				assert_eq!(ret_val, 0);
+			}
+		};
+		($name: ident, $str1: expr, $str2: expr) => {
+			#[test]
+			fn $name() {
+				let s1 = CString::new($str1).expect("Cannot create first string");
+				let s2 = CString::new($str2).expect("Cannot create second string");
+				let ret_val = unsafe {
+					ft_strcmp(s1.into_raw(), s2.into_raw())
+				};
+				assert_ne!(ret_val, 0);
+			}
+		};
+	}
+
+	// Matching
+	test!(basic, "SuperTest");
+	test!(longer, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ornare et ipsum et molestie. Sed fermentum metus ut sem imperdiet pretium. Etiam non dolor justo. Nullam dignissim malesuada dui, a malesuada ex facilisis ac. Nullam sit amet odio et neque vestibulum eleifend. Etiam malesuada ultrices orci. Sed quam ligula, pharetra at mattis vitae, mollis et urna. Proin a lobortis elit. Quisque gravida nec lorem ut auctor. In vitae tincidunt arcu. Cras ultricies augue augue, in mattis massa elementum vel.");
+	test!(utf8, "Salut! C'est un test de qualité contenant de supers UTF-8. 🀄麻雀🀄がしたい。このテストは本当に面白いなぁ。");
+
+	// Mismatch
+	test!(basic_mismatch, "SuperTest", "SuperTeste");
+	test!(longer_mismatch, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ornare et ipsum et molestie. Sed fermentum metus ut sem imperdiet pretium. Etiam non dolor justo. Nullam dignissim malesuada dui, a malesuada ex facilisis ac. Nullam sit amet odio et neque vestibulum eleifend. Etiam malesuada ultrices orci. Sed quam ligula, pharetra at mattis vitae, mollis et urna. Proin a lobortis elit. Quisque gravida nec lorem ut auctor. In vitae tincidunt arcu. Cras ultricies augue augue, in mattis massa elementum vel.", "Lorme ipsum dolor sit amet, consectetur adipiscing elit. Integer ornare et ipsum et molestie. Sed fermentum metus ut sem imperdiet pretium. Etiam non dolor justo. Nullam dignissim malesuada dui, a malesuada ex facilisis ac. Nullam sit amet odio et neque vestibulum eleifend. Etiam malesuada ultrices orci. Sed quam ligula, pharetra at mattis vitae, mollis et urna. Proin a lobortis elit. Quisque gravida nec lorem ut auctor. In vitae tincidunt arcu. Cras ultricies augue augue, in mattis massa elementum vel.");
+	test!(utf8_mismatch, "Salut! C'est un test de qualité contenant de supers UTF-8. 🀄麻雀🀄がしたい。このテストは本当に面白いなぁ。", "Salut! C'est un test de qualité contenant de supers UTF-8. 🀓麻雀🀄がしたい。このテストは本当に面白いなぁ。");
+}
+
+mod write {
+    use std::ffi::CString;
+    use cty::c_void;
+
+    use crate::ft_write;
+
+	macro_rules! test {
+		($name: ident, $to_test: expr) => {
+			#[test]
+			fn $name() {
+				let fd = 1;
+				let buffer = CString::new($to_test).expect("Couldn't create string");
+				let length = $to_test.len();
+				let ret_val = unsafe {
+					ft_write(fd, buffer.into_raw() as *mut c_void, length)
+				};
+				assert_eq!(ret_val, length as isize);
+			}
+		};
+	}
+
+	test!(basic, "SuperTest\n");
+	test!(longer, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ornare et ipsum et molestie. Sed fermentum metus ut sem imperdiet pretium. Etiam non dolor justo. Nullam dignissim malesuada dui, a malesuada ex facilisis ac. Nullam sit amet odio et neque vestibulum eleifend. Etiam malesuada ultrices orci. Sed quam ligula, pharetra at mattis vitae, mollis et urna. Proin a lobortis elit. Quisque gravida nec lorem ut auctor. In vitae tincidunt arcu. Cras ultricies augue augue, in mattis massa elementum vel.\n");
+	test!(utf8, "Salut! C'est un test de qualité contenant de supers UTF-8. 🀄麻雀🀄がしたい。このテストは本当に面白いなぁ。\n");
+}
+
+mod read {
+    use std::ffi::CString;
+    use cty::c_void;
+
+    use crate::ft_read;
+
+	macro_rules! test {
+		($name: ident, $to_test: expr) => {
+		};
+	}
+
+	#[test]
+	fn basic() {
+		let fd = 1;
+		let mut buffer = [0_i8; 10];
+		let length = 10;
+		let ret_val = unsafe {
+			ft_read(fd, buffer.as_mut_ptr() as *mut c_void, length)
+		};
+		assert_eq!(ret_val, length as isize);
+	}
+
+	test!(basic, "SuperTest\n");
+	test!(longer, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ornare et ipsum et molestie. Sed fermentum metus ut sem imperdiet pretium. Etiam non dolor justo. Nullam dignissim malesuada dui, a malesuada ex facilisis ac. Nullam sit amet odio et neque vestibulum eleifend. Etiam malesuada ultrices orci. Sed quam ligula, pharetra at mattis vitae, mollis et urna. Proin a lobortis elit. Quisque gravida nec lorem ut auctor. In vitae tincidunt arcu. Cras ultricies augue augue, in mattis massa elementum vel.\n");
+	test!(utf8, "Salut! C'est un test de qualité contenant de supers UTF-8. 🀄麻雀🀄がしたい。このテストは本当に面白いなぁ。\n");
 }
