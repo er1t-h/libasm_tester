df5050d449db520d98bfb5a22ec2831a22f844db
hperrin <hperrin@student.42.fr>
2023-03-14 21:01:27 +0100

    Finished tester

--- bindgen/libasm.h
+++ bindgen/libasm.h
@@ -21,8 +21,9 @@ int		ft_atoi_base(const char *str, const char *base);
 void	ft_list_push_front(t_list **begin, void *data);
 size_t	ft_list_size(t_list *begin);
 void	ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)(const void *, const void *), void (*free_fct)(void *));
-void	ft_list_sort(t_list **begin_list, int (*cmp)());
+void	ft_list_sort(t_list **begin_list, int (*cmp)(const void *, const void *));
 
 // Utils
 int		compare_first_letter(const void *s1, const void *s2);
+int		strcmp_wrapper(const void *s1, const void *s2);
 void	no_free(void *mem);
