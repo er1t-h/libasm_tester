0c364427b1d087e63cb7aaf3bc18d50bcee8c7a7
hperrin <hperrin1@proton.me>
2024-11-23 11:12:45 +0900

    Added some tests for atoi_base Improved README

--- README.md
+++ README.md
@@ -16,11 +16,13 @@ dynamic_lib:
     cc -shared -o libasm.o ${OBJS}
 ```
 3. Run `make dynamic_lib`. If it fails, you can lecture them about how it's important to create [Position-Independent Code](https://en.wikipedia.org/wiki/Position-independent_code).
-    - If you feel generous enough, add `wrt ..plt` after every `call [function]` instruction they use, and proceed
-4. Run `./libasm_tester`. If there's a lot of red, it might be because the person you correct didn't do the bonus, in which case you can run `./libasm_tester mandatory`.
+    - If you feel generous enough, add `wrt ..plt` after every `call [function]` instruction they use, and proceed.
+4. Run `./libasm_tester`. If there's a lot of red, it might be because the person you correct didn't implement the bonus, in which case you can run `./libasm_tester mandatory`.
+    - If *every single test fails* (with a lot of mention of "poisoning"), it might be because it didn't find the `libasm.so` file. Ensure the tester is placed in the same directory as the `.so`.
 5. To run valgrind (because you definitely want to run valgrind), use this command:
 ```sh
 valgrind --leak-check=full --trace-children=yes --show-leak-kinds=all --suppressions=./valgrind_suppression_files/patch_rusty_and_cargo_test.txt ./libasm_tester mandatory
 ```
 
 6. **If a test fails, and the error message contains "DPS:", it's not the student fault**. Therefore, Don't Penalize the Student.
+7. If a test fails, and the student is to blame, you can check which test is run. It will be located in the `src/test/(mandatory|bonus)/` of the repository.
--- src/tests/bonus/atoi_base.rs
+++ src/tests/bonus/atoi_base.rs
@@ -7,8 +7,12 @@ macro_rules! test {
         crate::fork_test! {
             #[test]
             fn $name() {
-                let str = CString::new($str).expect("DPS: Cannot create str");
-                let base = CString::new($base).expect("DPS: Cannot create base");
+                let s = $str;
+                let b = $base;
+                let str = CString::new(s).expect("DPS: Cannot create str");
+                let base = CString::new(b).expect("DPS: Cannot create base");
+                eprintln!("input string: {}", s);
+                eprintln!("input base: {}", b);
                 let ret_val = unsafe { libasm::ft_atoi_base(str.as_ptr(), base.as_ptr()) };
                 assert_eq!(ret_val, $result);
             }
@@ -68,11 +72,17 @@ test!(
     0
 );
 test!(space_in_base, "123", "01234 56789", 0);
-test!(tab_in_base, "123", "01234\t56789", 0);
-test!(carriage_return_in_base, "123", "01234\r56789", 0);
+test!(form_feed_in_base, "123", "0123456789\x0c", 0);
 test!(newline_in_base, "123", "01234\n56789", 0);
-test!(nothing_in_base, "Super", "", 0);
-test!(nothing_in_str, "", "0123456789", 0);
+test!(carriage_return_in_base, "123", "01234\r56789", 0);
+test!(horizontal_tab_in_base, "123", "01234\t56789", 0);
+test!(vertical_tab_in_base, "123", "0123456\x0b789", 0);
+test!(empty_base, "Super", "", 0);
+test!(base_of_size_one, "Super", "0", 0);
+test!(empty_string, "", "0123456789", 0);
+test!(nothing_to_parse, "abcd", "0123456789", 0);
+test!(int_min, "-2147483648oui", "0123456789", -2147483648);
+test!(negative_number_with_base_6, "-deadbeef", "abcdef", -1030709);
 
 // How to add tests:
 // `test!(name_of_the_test, "string to atoi", "the base", expected_result)`
